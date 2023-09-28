5ae3fd72503b2b449d2a2b886b3e85605789dbb4
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-09-28 12:19:39 -0500

    test

--- src/main/java/Q1/LP4_4.java
+++ src/main/java/Q1/LP4_4.java
@@ -2,6 +2,7 @@ package Q1;
 import java.util.*;
 public class LP4_4 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Random rand = new Random();
         int one = rand.nextInt(1,11);
         int two = rand.nextInt(1,11);
@@ -12,6 +13,27 @@ public class LP4_4 {
             operator = "+";
             answer= one + two ;
         }
+        if(op == 2){
+            operator = "-";
+            answer= one - two ;
+        }
+        if(op == 3){
+            operator = "*";
+            answer= one * two ;
+        }
+        if(op == 4){
+            operator = "/";
+            answer= one / two ;
+        }
+        System.out.println(one + " " + operator + " "+ two);
+        System.out.println("Whats the answer?");
+        int user = input.nextInt();
+        if(user==answer)System.out.println("Correct!");
+        else{
+            System.out.println("WRONG");
+        }
+
+
 
     }
 }
