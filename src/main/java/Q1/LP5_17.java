0e101503277dae1e782297456abd2b02e4bd5fc7
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-10-10 11:21:06 -0500

    up

--- src/main/java/Q1/LP5_17.java
+++ src/main/java/Q1/LP5_17.java
@@ -5,10 +5,19 @@ public class LP5_17 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String Pass = "Computer1776!";
-        String enter = input.nextLine();
-        if (Pass.equals(enter)) System.out.print("CORRECT!");
-        else{
-            System.out.println("WRONG");
+        int lcv = 0;
+        while(lcv<3){
+            System.out.println("Enter Password");
+            String enter = input.nextLine();
+            if (!Pass.equals(enter)){
+                System.out.println("Incorrect");
+                lcv++;
+                if(lcv==3) System.out.println("Acess denied");
+            }
+            else if (Pass.equals(enter)){
+                System.out.println("Acess Granted");
+                lcv=4;
+            }
         }
 
     }
--- src/main/java/Q1/Prog285bCL/Salesperson.java
+++
@@ -1,4 +0,0 @@
-package Q1.Prog285bCL;
-
-public class Salesperson {
-}
