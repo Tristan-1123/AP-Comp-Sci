4fc9b43aa5d15733eb632d5ed0d6d0ec96cca450
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-10-18 11:35:21 -0500

    up

--- src/main/java/Q1/Prog209a/Prog209Cl.java
+++ src/main/java/Q1/Prog209a/Prog209Cl.java
@@ -3,19 +3,21 @@ package Q1.Prog209a;
 public class Prog209Cl {
     private int less;
     private int more;
-    private int numson;
+    private int input;
 
     public Prog209Cl(int num){
-        numson=num;
+        input=num;
         less=0;
         more=0;
     }
-    public void calc(){
-        if(numson<500)less+=1;
+    private void calc(){
+        if(input<500)less+=1;
         else more+=1;
     }
-    public String toString(){
-        return "Numbers less than 500: "+ less + "numbers equal to or greater than 500: "+ more;
+    public void amek(){
+        calc();
     }
+    public int getLess(){return less;}
+    public int getMore(){return more;}
 
 }
--- src/main/java/Q1/Prog209a/Prog209a.java
+++ src/main/java/Q1/Prog209a/Prog209a.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 import java.util.*;
 public class Prog209a {
     public static void main(String[] args) {
+        int num =0;
+        int mo =0;
+        int le =0;
         try {
-            Scanner input = new Scanner(new File("Langdat/prog285b.dat"));
-
+            Scanner input = new Scanner(new File("Langdat/prog215a.txt"));
             while (input.hasNext()) {
-                int num=
-
+                num= input.nextInt();
+                Prog209Cl res = new Prog209Cl(num);
+                res.amek();
+                 mo += res.getMore();
+                 le += res.getLess();
             }
+            System.out.println("More than 500: " + mo + "\n Less than: "+ le);
+
         } catch (IOException e) {
             System.out.println("Can't find Data File!");
         }
