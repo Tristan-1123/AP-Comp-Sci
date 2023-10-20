fb5f0ad9be9643d7cbb731438566973257056d6c
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-10-20 11:20:28 -0500

    up

--- src/main/java/Q1/Prog213e/Prog213e.java
+++ src/main/java/Q1/Prog213e/Prog213e.java
@@ -1,4 +1,39 @@
 package Q1.Prog213e;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 public class Prog213e {
-}
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("Langdat/prog213e.dat"));
+            int loopcv = input.nextInt();
+            int lcv = 0;
+            int num = 0;
+            int num1=0;
+            int num2=0;
+            int num3=0;
+            int num4=0;
+            int num5=0;
+            while (input.hasNext() & lcv<=loopcv){
+                num = input.nextInt();
+                Prog213eCl nu = new Prog213eCl(num);
+                nu.calc();
+                num1 += nu.getAge1();
+                num2 += nu.getAge2();
+                num3 += nu.getAge3();
+                num4 += nu.getAge4();
+                num5 += nu.getAge5();
+                lcv++;
+            }
+            System.out.println(loopcv);
+            System.out.println("<20 \t"+ num1 + "\t" + (num1/loopcv) +"\n"+
+                    "20-39 \t"+ num2 + "\t" + (num2/loopcv)+ "\n"+
+                    "40-59 \t"+ num3 + "\t" + (num3/loopcv) + "\n"+
+                    "60-79 \t" + num4 + "\t" + (num4/loopcv) + "\n"+
+                    ">80 \t" + num5 + "\t" + (num5/loopcv));
+            }
+        catch (IOException e) {
+            System.out.println("Can't find Data File!");
+
+    }
+}}
--- src/main/java/Q1/Prog213e/Prog213eCl.java
+++ src/main/java/Q1/Prog213e/Prog213eCl.java
@@ -1,10 +1,32 @@
 package Q1.Prog213e;
 
 public class Prog213eCl {
+    private int age;
     private int age1;
     private int age2;
     private int age3;
     private int age4;
     private int age5;
 
+    public Prog213eCl(int ageinput){
+        age = ageinput;
+        age1=0;
+        age2=0;
+        age3=0;
+        age4=0;
+        age5=0;
+    }
+    public void calc(){
+        if (age<20) age1++;
+        else if (age >= 20 && age<=39) age2++;
+        else if (age >= 40 && age<=59) age3++;
+        else if (age >= 60 && age<=79) age4++;
+        else if (age>=80) age5++;
+    }
+
+    public int getAge1(){return age1;}
+    public int getAge2(){return age2;}
+    public int getAge3(){return age3;}
+    public int getAge4(){return age4;}
+    public int getAge5(){return age5;}
 }
