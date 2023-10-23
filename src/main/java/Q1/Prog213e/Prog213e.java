2c5f59d8b78c1fdd02b549ef7dd2226ded530dab
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-10-23 11:07:13 -0500

    up

--- src/main/java/Q1/Prog213e/Prog213e.java
+++ src/main/java/Q1/Prog213e/Prog213e.java
@@ -6,7 +6,7 @@ public class Prog213e {
     public static void main(String[] args) {
         try{
             Scanner input = new Scanner(new File("Langdat/prog213e.dat"));
-            int loopcv = input.nextInt();
+            double loopcv = input.nextDouble();
             int lcv = 0;
             int num = 0;
             int num1=0;
@@ -14,7 +14,7 @@ public class Prog213e {
             int num3=0;
             int num4=0;
             int num5=0;
-            while (input.hasNext() & lcv<=loopcv){
+            while (input.hasNext() & lcv<loopcv){
                 num = input.nextInt();
                 Prog213eCl nu = new Prog213eCl(num);
                 nu.calc();
@@ -25,12 +25,11 @@ public class Prog213e {
                 num5 += nu.getAge5();
                 lcv++;
             }
-            System.out.println(loopcv);
-            System.out.println("<20 \t"+ num1 + "\t" + (num1/loopcv) +"\n"+
-                    "20-39 \t"+ num2 + "\t" + (num2/loopcv)+ "\n"+
-                    "40-59 \t"+ num3 + "\t" + (num3/loopcv) + "\n"+
-                    "60-79 \t" + num4 + "\t" + (num4/loopcv) + "\n"+
-                    ">80 \t" + num5 + "\t" + (num5/loopcv));
+            System.out.println("<20 \t"+ num1 + "\t" + (num1/loopcv) +"%\n"+
+                    "20-39 \t"+ num2 + "\t" + (num2/loopcv)+ "%\n"+
+                    "40-59 \t"+ num3 + "\t" + (num3/loopcv) + "%\n"+
+                    "60-79 \t" + num4 + "\t" + (num4/loopcv) + "%\n"+
+                    ">80 \t" + num5 + "\t" + (num5/loopcv) + "%");
             }
         catch (IOException e) {
             System.out.println("Can't find Data File!");
