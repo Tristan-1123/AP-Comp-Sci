64d4af1b75c5e04b8f015dca0993bbec1c556e37
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-11-08 11:36:58 -0600

    up

--- src/main/java/Q2/Prog435aCl/Cl435a.java
+++ src/main/java/Q2/Prog435aCl/Cl435a.java
@@ -1,20 +1,53 @@
 package Q2.Prog435aCl;
 
 public class Cl435a {
-    private int vehicle;
-    private int gate;
+    private int car;
+    private int ga;
     private double type;
     private String car_type;
     private double toll;
 
-    public Cl435a(int car, int ga){
-        vehicle = car;
-        gate = ga;
+    public Cl435a(int vehicle, int gate){
+        car = vehicle;
+        ga = gate;
         type = 0;
         car_type = "";
         toll = 0;
-
-
-
     }
+    public void calc(){
+        if (car == 1){
+            type =1;
+            car_type= "Compact Car";
+        }
+        else if (car == 2) {
+            type = 1.3;
+            car_type= "Small Car";
+        }
+        else if (car == 3){
+            type = 1.6;
+            car_type="Mid Size Car";
+        }
+        else if(car == 4){
+            type = 2;
+            car_type = "Full Size Car";
+        }
+        else if(car ==5) {
+            type=2.4;
+            car_type = "Truck";
+        }
+        else if (car == 6){
+            type =2.7;
+            car_type ="16 Wheeler";
+        }
+
+        if (ga ==1) toll = 1.35;
+        else if (ga == 2) toll = 2;
+        else if (ga == 3) toll = 2.5;
+        else if (ga == 4) toll = 3.25;
+        else if (ga == 5) toll = 4.1;
+        else if (ga == 6) toll = 4.8;
+        else if (ga == 7) toll = 5.5;
+        else if (ga == 8) toll = 6;
+    }
+    public String toString() {return "\t"+car_type +"\t"+ toll + "\t"+ type + "\t"+ "$" + (toll*type);}
 }
--- src/main/java/Q2/Prog435aCl/Prog435a.java
+++ src/main/java/Q2/Prog435aCl/Prog435a.java
@@ -1,4 +1,26 @@
 package Q2.Prog435aCl;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Prog435a {
-}
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("Langdat/prog435a.dat"));
+            int [] list = new int[300];
+            int count = 0;
+
+            while (input.hasNext()){
+                int num = input.nextInt();
+                list[count] = num;
+                count++;
+            }
+            for (int lcv = 0; lcv < list.length; lcv++) {
+                Cl435a fin = new Cl435a(list[lcv],list[lcv+1]);
+                System.out.println(fin.toString());
+
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Can't find Data File!");
+}}}
