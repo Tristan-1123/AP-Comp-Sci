e382dac195962496c0d0c0a39bc4e44ed723623d
Tristan-1123 <meier.t@stu.Janesville.k12.wi.us>
2023-11-15 11:33:03 -0600

    up

--- src/main/java/Q2/Prog214bCl/Cl214b.java
+++ src/main/java/Q2/Prog214bCl/Cl214b.java
@@ -23,6 +23,21 @@ public class Cl214b {
     }
     public void calc(){
         gross = hours*(b_p_r*s_c);
+        if (gross <100)with = 0;
+        else if (gross <149.99 && gross >100)with = 0.08;
+        else if (gross <199 && gross >150) with = 0.12;
+        else if (gross < 200 && gross>299.99)with = 0.15;
+        else with = 0.17;
+        with *= gross;
+        if (y_t_d>17300)soc_tax = 0;
+        else if (y_t_d<=17300 && (y_t_d+gross)>17300) soc_tax = ((y_t_d+gross)-17300)*0.0605;
+        else soc_tax = gross*0.0605;
+        net_pay = (gross - with) - soc_tax;
 
+    }
+    public String toString(){
+        return "Employee Numner: "+ em_num+ "\n"+ "Hours: "+ hours+ "\t"+ "rate: " + b_p_r+ "\t"+ "Shift Factor: " + s_c
+                + " \n"+ "Gross Pay: " +"\t"+ gross+"\t" + "YTD: " +y_t_d+"\n" + "Witholding : " + with+"\n"+ "FICA: " +soc_tax+
+                "\n" + "net pay: "+ net_pay;
     }
 }
--- src/main/java/Q2/Prog214bCl/Prog214b.java
+++ src/main/java/Q2/Prog214bCl/Prog214b.java
@@ -1,4 +1,33 @@
 package Q2.Prog214bCl;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Prog214b {
-}
+    public static void main(String[] args) {
+        try{
+            Scanner input = new Scanner(new File("Langdat/prog214b.txt"));
+            int [] num1 = new int[100];
+            double [] num2 = new double[100];
+            double [] num3 = new double[100];
+            double [] num4 = new double[100];
+            double [] num5 = new double[100];
+            int cnt=0;
+            while (input.hasNext()){
+                num1[cnt]= input.nextInt();
+                num2[cnt]= input.nextDouble();
+                num3[cnt]= input.nextDouble();
+                num4[cnt]= input.nextDouble();
+                num5[cnt]= input.nextDouble();
+                cnt++;
+            }
+            for(int lcv = 0; lcv<cnt; lcv++){
+                Cl214b yunn = new Cl214b(num1[lcv], num2[lcv],num3[lcv],num4[lcv],num5[lcv]);
+                yunn.calc();
+                System.out.println("\n"+"\n");
+                System.out.println(yunn);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Can't find Data File!");
+}}}
